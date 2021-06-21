0be9ca71e2ec032f885a4e0f281db7f0239844b4
kombuchan <76601670+kombuchan@users.noreply.github.com>
2021-06-21 10:04:11 -0700

    Added input validation for registration

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route("/")
-def homepage():
-	return render_template("homepage.html")
+@app.route("/home")
+def home():
+	return render_template("home.html")
 
-if __name__ == "__main__":
-	app.run(debug="True")
+@app.route("/stats")
+def stats():
+	return render_template("stats.html")
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
