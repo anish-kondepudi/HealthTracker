b376d9849560cb3ed4bb1f52426bac38fb4f8f5f
anish-kondepudi <anish.kondepudi@gmail.com>
2021-06-21 10:30:10 -0700

    Updated main.py to Mihika's Code for Login Validation

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy # Use for Databasing in Future
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route("/")
 @app.route("/home")
 def home():
-	return render_template("home.html")
+    return render_template("home.html")
 
 @app.route("/stats")
 def stats():
-	return render_template("stats.html")
-
-@app.route("/login")
-def login():
-	return render_template("login.html")
-
-@app.route("/register")
+    return render_template("stats.html")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-	return render_template("register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
 
-if __name__ == "__main__":
-	app.run(debug="True")
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
