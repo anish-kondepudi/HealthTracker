6bc381c10419994fa781de1e1dd89a1f55edafd6
kombuchan <76601670+kombuchan@users.noreply.github.com>
2021-06-21 20:49:51 -0700

    worked more on registration page

--- forms.py
+++ forms.py
@@ -5,12 +5,12 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Username"})
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+                        validators=[DataRequired(), Email()],render_kw={"placeholder": "Email"})
+    password = PasswordField('Password', validators=[DataRequired()],render_kw={"placeholder": "Password"})
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired()])
+                                     validators=[DataRequired(), EqualTo('password')],render_kw={"placeholder": "Confirm Password"})
     submit = SubmitField('Sign Up')
 
 
