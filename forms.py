424bc59c5768fe8617bf38f29d7ab71e5d678d36
kombuchan <76601670+kombuchan@users.noreply.github.com>
2021-06-23 15:57:34 -0700

    new commit

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
@@ -19,4 +20,27 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
+=======
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Username"})
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()],render_kw={"placeholder": "Email"})
+    password = PasswordField('Password', validators=[DataRequired()],render_kw={"placeholder": "Password"})
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password','Passwords must match')],render_kw={"placeholder": "Confirm Password"})
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+>>>>>>> Stashed changes
     submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 
@@ -37,3 +38,44 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+=======
+from flask import Flask, render_template, url_for, flash, redirect, request
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+@app.route("/")
+@app.route("/home")
+def home():
+	return render_template("home.html")
+
+@app.route("/stats")
+def stats():
+	return render_template("stats.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # print("yo")
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('register'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+>>>>>>> Stashed changes
