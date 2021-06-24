4a47b1c0a58ea979dd07ba02c5972d73c4027bf6
kombuchan <76601670+kombuchan@users.noreply.github.com>
2021-06-24 16:09:05 -0700

    Set up database for users

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='default.jpg') # use hash
+    password = db.Column(db.String(60), nullable=False) # use hash
+
+    def __repr__(self) :
+        return f"User('{self.username}','{self.email}','{self.image}')"
+
 
 @app.route("/")
 @app.route("/home")
@@ -17,7 +32,6 @@ def stats():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        # print("yo")
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('register'))
     return render_template('register.html', title='Register', form=form)
@@ -29,6 +43,7 @@ def login():
     if form.validate_on_submit():
         if form.email.data == 'admin@admin.com' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
+            # flash(f'Welcome back, {form.username.data}!', 'success')
             return redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
