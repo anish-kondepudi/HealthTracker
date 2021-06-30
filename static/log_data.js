84810f70ed09400197c216f7a4407daa85464968
anish-kondepudi <anish.kondepudi@gmail.com>
2021-06-29 21:24:47 -0700

    Added basic JS to unhide HTML upon sequential completion of log data form

---
+++ static/log_data.js
@@ -0,0 +1,55 @@
+function fieldComplete1() {
+  var x = document.getElementById("sleep");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+function fieldComplete2() {
+  var x = document.getElementById("workout");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+function fieldComplete3() {
+  var x = document.getElementById("healthy");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+function fieldComplete4() {
+  var x = document.getElementById("workoutLength");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+function fieldComplete5() {
+  var x = document.getElementById("typeWorkout");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+function fieldComplete6() {
+  var x = document.getElementById("unhealthyFood");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+function fieldComplete7() {
+  var x = document.getElementById("proudAchievement");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
+
+function fieldComplete8() {
+  var x = document.getElementById("submitButton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+}
