e61683b7c5d2de4f76899c527dd3e7e2f48a9821
anish-kondepudi <anish.kondepudi@gmail.com>
2021-07-02 19:29:11 -0700

    Porting Mihika's Branch to Mine

--- static/log_data.js
+++ static/log_data.js
@@ -12,42 +12,50 @@ function fieldComplete2() {
   }
 }
 
-function fieldComplete3() {
+function fieldComplete3(condition) {
+  var x = document.getElementById("healthy");
+  var y = document.getElementById("typeWorkout");
+  var z = document.getElementById("workoutLength");
+  if (condition == 0) {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "none";
+  }
+  else if (condition == 1) {
+    x.style.display = "none";
+    y.style.display = "block";
+    z.style.display = "block";
+  }
+}
+
+function fieldComplete4() {
   var x = document.getElementById("healthy");
   if (x.style.display === "none") {
     x.style.display = "block";
   }
 }
 
-function fieldComplete4() {
-  var x = document.getElementById("workoutLength");
-  if (x.style.display === "none") {
+function fieldComplete5(condition) {
+  var x = document.getElementById("unhealthyFood");
+  var y = document.getElementById("proudAchievement");
+  if (x.style.display === "none" && condition == 0) {
     x.style.display = "block";
+    y.style.display = "none";
   }
-}
-
-function fieldComplete5() {
-  var x = document.getElementById("typeWorkout");
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  else if (y.style.display === "none" && condition == 1) {
+    x.style.display = "none";
+    y.style.display = "block";
   }
 }
 
 function fieldComplete6() {
-  var x = document.getElementById("unhealthyFood");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  }
-}
-
-function fieldComplete7() {
   var x = document.getElementById("proudAchievement");
   if (x.style.display === "none") {
     x.style.display = "block";
   }
 }
 
-function fieldComplete8() {
+function fieldComplete7() {
   var x = document.getElementById("submitButton");
   if (x.style.display === "none") {
     x.style.display = "block";
