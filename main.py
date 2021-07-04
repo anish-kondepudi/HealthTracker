12893ba3796947d412c50525702f6cad75f57819
anish-kondepudi <anish.kondepudi@gmail.com>
2021-07-04 15:41:23 -0700

    Added comments on things to fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 app.secret_key = "x{RD/'wutjN87mGN/acnEPSqkS4wp_"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' # change name to something more representative
 
 db = SQLAlchemy(app)
 
